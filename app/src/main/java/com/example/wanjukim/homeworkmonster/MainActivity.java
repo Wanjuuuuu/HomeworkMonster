c0caea97c65a93579a59a358732b5742bde9a26d
2013034511 <makarong5798@gmail.com>
2017-11-01 12:44:50 +0900

    Add action bar and resources

--- app/src/main/java/com/example/wanjukim/homeworkmonster/MainActivity.java
+++ app/src/main/java/com/example/wanjukim/homeworkmonster/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.wanjukim.homeworkmonster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //when option is selected
+
+        return super.onOptionsItemSelected(item);
     }
 }
